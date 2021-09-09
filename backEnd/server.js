566af4ea848ba9bf9f8e64f37e00a7dcc03eb8e9
JenniferSilva25 <silvajennifer2509@gmail.com>
2021-09-09 20:27:34 -0300

    adding methods for borrowing

--- backEnd/server.js
+++ backEnd/server.js
@@ -8,8 +8,8 @@ app.use(express.json());
 const port = 8080;
 
 const userMongo = require('./src/controller/crud_user_controller');
-const book =  require('./src/controller/crud_book_controller');
-
+const book = require('./src/controller/crud_book_controller');
+const book_loan = require('./src/controller/book_loan')
 app.post('/user', userMongo.createUser);
 app.get('/user/', userMongo.getUser);
 app.put('/user', userMongo.updateUser);
@@ -19,7 +19,10 @@ app.get('/user/login', userMongo.loginUser);
 app.post('/book', book.createdBook);
 app.get('/book', book.getBook);
 app.put('/book', book.updateBook);
-app.del('/book', book.deleteBook);
+app.delete('/book', book.deleteBook);
 
-
+app.post('/loan/sender', book_loan.createSender);
+app.post('/loan/addressee', book_loan.createAddressee);
+app.post('/loan/addLoan', book_loan.addLoan);
+app.delete('/loan/delete', book_loan.deleteMatch);
 app.listen(port, () => console.log(port));
---
+++ backEnd/src/controller/book_loan.js
@@ -0,0 +1,83 @@
+ const {
+     session
+ } = require('../database/neo4j');
+
+ const createSender = async (req, res) => {
+
+     const obj = {
+         idUser: req.body.idUser,
+         nameUser: req.body.nameUser,
+         idBook: req.body.idBook,
+         titleBook: req.body.titleBook
+     };
+     await session.run('CREATE (p:sender{idUser:$idUser, nameUser:$nameUser, idBook:$idBook, titleBook:$titleBook}) RETURN p', {
+             idUser: obj.idUser,
+             nameUser: obj.nameUser,
+             idBook: obj.idBook,
+             titleBook: obj.titleBook
+         }).then(res.send('User created to success'))
+         .catch(error => console.log(error))
+         .then(() => {
+             session.close
+         });
+ }
+ const createAddressee = async (req, res) => {
+
+     const obj = {
+         idUser: req.body.idUser,
+         nameUser: req.body.nameUser,
+     };
+     await session.run('CREATE (p:addressee{idUser:$idUser, nameUser:$nameUser}) RETURN p', {
+             idUser: obj.idUser,
+             nameUser: obj.nameUser,
+         }).then(console.log(res.send('User created to success')))
+         .catch(error => console.log(error))
+         .then(() => {
+             session.close
+         });
+ }
+
+ const addLoan = async (req, res) => {
+     const obj = {
+         idBook: req.body.idBook,
+         idUser: req.body.idUser
+     };
+     await session.run('MATCH (p1:sender{idBook:$idBook}) MATCH (p2:addressee{idUser:$idUser}) CREATE (p1)-[:LOAN]->(p2)', {
+             idBook: obj.idBook,
+             idUser: obj.idUser
+         }).then(result => {
+             if (result.summary.counters._stats.relationshipsCreated > 0) {
+                 res.send("success");
+             } else {
+                 console.log(res.send("Fail"))
+             }
+         })
+         .catch(error => console.log(error))
+         .then(() => {
+             session.close
+         });
+ }
+
+ const deleteMatch = async (req, res) => {
+     const obj = {
+         idBook: req.body.idBook,
+         idUser: req.body.idUser
+     };
+     await session.run('MATCH (p1:sender{idBook:$idBook})-[p2]-(p3:addressee{idUser:$idUser}) DELETE p1,p2,p3', {
+             idBook: obj.idBook,
+             idUser: obj.idUser
+         }).then(result => {
+             res.send("success");
+         })
+         .catch(error => console.log(error))
+         .then(() => {
+             session.close
+         });
+ }
+
+ module.exports = {
+     createSender,
+     createAddressee,
+     addLoan,
+     deleteMatch
+ }
