0604cd6632cc2b856279807a31d0321096318592
camilacasimiro <camilacasimiiro@gmail.com>
2021-09-09 17:13:37 -0300

    Adding routes referring to the book

--- backEnd/server.js
+++ backEnd/server.js
@@ -8,11 +8,18 @@ app.use(express.json());
 const port = 8080;
 
 const userMongo = require('./src/controller/crud_user_controller');
+const book =  require('./src/controller/crud_book_controller');
 
 app.post('/user', userMongo.createUser);
 app.get('/user/', userMongo.getUser);
 app.put('/user', userMongo.updateUser);
 app.delete('/user/', userMongo.delUser);
 app.get('/user/login', userMongo.loginUser);
+
+app.post('/book', book.createdBook);
+app.get('/book', book.getBook);
+app.put('/book', book.updateBook);
+app.del('/book', book.deleteBook);
+
 
 app.listen(port, () => console.log(port));
