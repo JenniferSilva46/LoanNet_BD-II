68b65d48a3efdac94c470d129c62ed7aee21ee01
camilacasimiro <camilacasimiiro@gmail.com>
2021-09-16 22:26:56 -0300

    modifying postegres database

--- backEnd/src/database/postgres.js
+++ backEnd/src/database/postgres.js
@@ -1,15 +1,15 @@
-const Pool = require('pg').Pool;
+const {Client} = require('pg');
 
-const pool = new Pool({
-    user: process.env.PG_USER,
+const client = new Client({
     host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
     database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD
 });
 
-pool.connect()
-    .then(()=> console.log('Conected postgis!'))
-    .catch(err => console.log(err.staccck));
+client.connect()
+    .then(()=> console.log('Conectado!'))
+    .catch(err => console.log(err.stack));
 
-module.exports = pool;
+module.exports= client
