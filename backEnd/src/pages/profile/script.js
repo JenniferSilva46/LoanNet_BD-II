6ebf4e5fa6e0ab115b96f3b978338d5dbcfbb34a
camilacasimiro <camilacasimiiro@gmail.com>
2021-09-19 23:19:12 -0300

    adding script from profile page

---
+++ backEnd/src/pages/profile/script.js
@@ -0,0 +1,33 @@
+
+const getId = () => {
+    fetch("http://localhost:8080/user/getUserId", {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      'Content-Type': 'application/json',
+      'x-acess-token': localStorage.getItem("token")
+    }
+  })
+  .then(res => res.json())
+  .then(data => {
+      createData(data);
+  })
+}
+
+getId()
+
+const createData = (data) => {
+    const dataDiv = document.createElement("div");
+    const name = document.createElement("p");
+    name.textContent = 'Nome: ' + data.name;
+    const email = document.createElement("p");
+    email.textContent = 'Email: ' + data.email;
+    const phone = document.createElement("p");
+    phone.textContent = 'Contato: ' + data.phone;
+
+    document.querySelector(".data-user").appendChild(dataDiv);
+    dataDiv.appendChild(name);
+    dataDiv.appendChild(email);
+    dataDiv.appendChild(phone);
+}
+
