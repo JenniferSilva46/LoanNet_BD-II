5c726b1ffd79f97c22e4f0cb8ab7d89fcfa70c9e
JenniferSilva46 <jennifer.silva@academico.ifpb.edu.br>
2021-09-24 20:27:34 -0300

    adding router

--- backEnd/server.js
+++ backEnd/server.js
@@ -19,7 +19,7 @@ const userMongo = require('./src/controller/crud_user_controller');
 const book = require('./src/controller/crud_book_controller');
 const book_loan = require('./src/controller/book_loan')
 app.post('/user/create', userMongo.createUser);
-app.get('/user/getUser', userMongo.getUser);
+app.get('/user/getUser/:email', userMongo.getUser);
 app.put('/user/update', userMongo.updateUser);
 app.delete('/user/deleteUse', userMongo.delUser);
 app.post('/user/login', userMongo.loginUser);
